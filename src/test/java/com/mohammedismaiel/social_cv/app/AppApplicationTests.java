c842e4ba65dc4eed49850b2a69045e74fe26200a
Mohammed_Ismaiel <mohammedismaiel333.z@gmail.com>
2022-12-16 15:06:58 +0200

    first commit

---
+++ src/test/java/com/mohammedismaiel/social_cv/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohammedismaiel.social_cv.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
